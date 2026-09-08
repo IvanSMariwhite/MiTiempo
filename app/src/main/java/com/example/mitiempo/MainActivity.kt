a4276a721eb5912e6367a841111f07aadc728631
IvanSMariwhite <ivansmariblanca@gmail.com>
2026-09-08 16:51:22 +0200

    SetUpInicial

--- app/src/main/java/com/example/mitiempo/MainActivity.kt
+++ app/src/main/java/com/example/mitiempo/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import com.example.mitiempo.ui.theme.MiTiempoTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,28 +23,26 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MiTiempoTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    WeatherScreen(
-                        modifier = Modifier
-                    )
-                }
+                WeatherScreen()
             }
         }
     }
 }
 
 @Composable
-fun WeatherScreen(modifier: Modifier = Modifier) {
+fun WeatherScreen() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Mi Tiempo")
-        Text(text = "🌤️")
-        Text(text = "25 °C")
-        Text(text = "Despejado")
+        Text(
+            text = "MiTiempo",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Text(
+            text = "Consulta el tiempo en cualquier lugar"
+        )
     }
 }
